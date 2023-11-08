714def5a7e0222f7919bde08975d75ae011b21e9
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2023-11-08 23:26:26 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,53 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#ifndef WEEK_4_ASSIGNMENT_LIFO_FIFO_DURDANA4_TASK_1_H
+#define WEEK_4_ASSIGNMENT_LIFO_FIFO_DURDANA4_TASK_1_H
+
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Stack {
+private:
+    Node* top;
+public:
+    Stack() {
+        top = NULL;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    void push(int value) {
+        Node* temp = new Node();
+        if (!temp) {
+            cout << "Heap Overflow" << endl;
+            exit(1);
+        }
+        temp->data = value;
+        temp->next = top;
+        top = temp;
+    }
+
+    void pop() {
+        if (top == NULL) {
+            cout << "Underflow" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    int topElement() {
+        if (!isEmpty())
+            return top->data;
+        else
+            exit(1);
+    }
+};
+#endif //WEEK_4_ASSIGNMENT_LIFO_FIFO_DURDANA4_TASK_1_H
